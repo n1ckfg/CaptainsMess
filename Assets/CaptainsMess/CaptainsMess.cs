d1585cbc8634e99b979f3ca55556d57c8a7bb2a7
Nick Fox-Gieg <nick@fox-gieg.com>
2017-10-24 20:54:18 -0400

    update to Unity 2017.2

--- Assets/CaptainsMess/CaptainsMess.cs
+++ Assets/CaptainsMess/CaptainsMess.cs
@@ -57,8 +57,8 @@ public class CaptainsMess : MonoBehaviour
 
     public void ValidateConfig()
     {
-        if (broadcastIdentifier == "Spaceteam" && !Application.bundleIdentifier.Contains("com.sleepingbeastgames"))
-        {
+        //if (broadcastIdentifier == "Spaceteam" && !Application.bundleIdentifier.Contains("com.sleepingbeastgames")) {
+        if (broadcastIdentifier == "Spaceteam" && !Application.identifier.Contains("com.sleepingbeastgames")) {
             Debug.LogError("#CaptainsMess# You should pick a unique Broadcast Identifier for your game", this);
         }
         if (playerPrefab == null)
